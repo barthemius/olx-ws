2dd82b2ff72a7d0ee705eb45e068d5480c7b27c3
Bartlomiej Kiczek <bartlomiej.kiczek@netrix.com.pl>
2022-07-31 16:28:51 +0200

    App foundations

---
+++ dash_app.py
@@ -0,0 +1,6 @@
+# Plotly dash app for creating a visualisation dashboard of the real estate data in Lublin
+# This is a simple dash app for visualising the data from the real estate market in Lublin.
+# The data is collected from olx.pl and otodom.pl.
+#
+
+from dash import Dash, callback_context, no_update
